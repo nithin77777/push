b9df54a025e0c75014bd0886a492eb46b09c4697
updated_data <nithinkrishna@capnxt.com>
2022-12-30 11:09:12 +0530

    updated

---
+++ output.sql
@@ -0,0 +1,18 @@
+CREATE TABLE battery_and_diesel (
+	`Timing` TEXT,
+	`EngineTempCelsius` TEXT,
+	`EngineTempFahrenheit` TEXT,
+	`Engine_oil_level` TEXT,
+	`RPM` FLOAT(53),
+	`Frequency_H2` FLOAT(53),
+	`Coollant_oil_level` TEXT,
+	`Battery_condition` TEXT,
+	`Diesel_level` TEXT,
+	`EngineTimeStart` TEXT,
+	`EngineTimeStop` TEXT,
+	`OutputVoltageR-Y` FLOAT(53),
+	`OutputVoltageY-B` TEXT,
+	`OutputVoltageB-R` TEXT,
+	`RunningMins` TEXT,
+	`Remarks` TEXT
+)
