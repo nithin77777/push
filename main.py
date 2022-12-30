74460d1941351c2ead682e5fa962cffdb715823e
updated_data <nithinkrishna@capnxt.com>
2022-12-30 11:33:27 +0530

    updated

--- main.py
+++ main.py
@@ -14,17 +14,23 @@ import chardet
 os.chdir('/Users/nithinsaikrishna/Downloads/')
 
 
-file = 'battery.csv'
+file = 'power_consumption.csv'
 with open(file, 'rb') as rawdata:
     result = chardet.detect(rawdata.read(100000))
 print(result)
 
 
-dataset = pd.read_csv('new_updated.csv',encoding='utf-8') #encoding='unicode_escape'
+dataset = pd.read_csv('power_consumption.csv',encoding='utf-8') #encoding='unicode_escape'
 # dataset.replace(to_replace=' ',value='NULL',inplace=True)
 print(dataset)
+dataset.to_csv('power_consumption_v1.csv', encoding='utf-8')
 
 db = create_engine("mysql://root:mnsk1315@127.0.0.1:3306/capnxt", echo=True)
 connection = db.connect()
-dataset.to_sql(name='battery_and_diesel',con=connection, if_exists='replace',index=False)
+dataset.to_sql(name='power_consumption',con=connection, if_exists='replace',index=False)
 connection.close()
+
+file = 'power_consumption_v1.csv'
+with open(file, 'rb') as rawdata:
+    result = chardet.detect(rawdata.read(100000))
+print('After conversion the file type is: ',result)
